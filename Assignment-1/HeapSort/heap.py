71eef06f65029df31ca2df010a23f96fbd4db27c
Aditya <adi.badayalya@gmail.com>
2021-08-24 10:48:26 +0530

    python

--- Assignment-1/HeapSort/heap.py
+++ Assignment-1/HeapSort/heap.py
@@ -1,25 +1,24 @@
 import random
-class Solution:
-   def heapify(self, arr, i, n):
-    largest = i
-    l = 2*i+1
-    r=2*i+2
-    if (l<n and arr[largest]<arr[l]):
-        largest = l
-    if (r<n and arr[largest]<arr[r]):
-        largest = r
-    if(largest != i):
-        arr[i],arr[largest]=arr[largest],arr[i]
-        self.heapify(arr, largest, n)    
-    
-   def HeapSort(self, arr, n):
-    for i in range(n//2 -1 ,-1,-1):
-        self.heapify(arr,i,n)
-    for i in range(n-1,0,-1):
-        arr[i],arr[0]=arr[0],arr[i]
-        self.heapify(arr,0,i)
+def heapify(arr, i, n):
+ largest = i
+ l = 2*i+1
+ r=2*i+2
+ if (l<n and arr[largest]<arr[l]):
+     largest = l
+ if (r<n and arr[largest]<arr[r]):
+     largest = r
+ if(largest != i):
+     arr[i],arr[largest]=arr[largest],arr[i]
+     heapify(arr, largest, n)    
+ 
+def HeapSort(arr, n):
+ for i in range(n//2 -1 ,-1,-1):
+     heapify(arr,i,n)
+ for i in range(n-1,0,-1):
+     arr[i],arr[0]=arr[0],arr[i]
+     heapify(arr,0,i)
 
 if __name__ == '__main__':
     arr = [random.randint(1,10000) for _ in range(100)]
-    Solution().HeapSort(arr,100)
+    HeapSort(arr,100)
     print(arr)
--- Assignment-1/QuickSort/quick.py
+++ Assignment-1/QuickSort/quick.py
@@ -1,23 +1,22 @@
 import random
-class Solution:
-    def partition(self, arr, l, r):
-        pivot=arr[r]
-        i =l-1
-        for j in range(l,r):
-            if(arr[j]<pivot):
-                i+=1
-                arr[j],arr[i]=arr[i],arr[j]
-        i+=1
-        arr[i],arr[r]=arr[r],arr[i]
-        return (i)
-    def quickSort(self, arr, l,r):
-        if(l<r):
-            pi = self.partition(arr,l,r)
-            self.quickSort(arr,l,pi-1)
-            self.quickSort(arr,pi+1,r)
+def partition(arr, l, r):
+    pivot=arr[r]
+    i =l-1
+    for j in range(l,r):
+        if(arr[j]<pivot):
+            i+=1
+            arr[j],arr[i]=arr[i],arr[j]
+    i+=1
+    arr[i],arr[r]=arr[r],arr[i]
+    return (i)
+def quickSort(arr, l,r):
+    if(l<r):
+        pi = partition(arr,l,r)
+        quickSort(arr,l,pi-1)
+        quickSort(arr,pi+1,r)
 
 
 if __name__ == '__main__':
     arr = [random.randint(1,10000) for _ in range(100)]
-    Solution().quickSort(arr,0,99)
+    quickSort(arr,0,99)
     print(arr)
